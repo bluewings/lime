b011fb4dbcf4e24b8805606272fae0353abc773c
bluewings <naver@naverui-MacBook-Pro.local>
2014-07-17 00:06:21 +0900

    fix thumbnail process

--- testgm.js
+++ testgm.js
@@ -1,54 +1,56 @@
 var fs = require('fs'),
     gm = require('gm');
 
-var testFile = './test/sample3.jpg';
+generateImage('./test/sample.jpg');
 
-var gmImg = gm(testFile);
-var THUMB_HEIGHT = 150;
+function generateImage(imgPath) {
 
-gmImg.size(function(err, src) {
+    var gmInst = gm(imgPath).autoOrient();
 
-    src.ratio = src.width / src.height;
-    var dst = {
-        thumbWidth: parseInt(THUMB_HEIGHT * src.width / src.height, 10),
-        thumbHeight: THUMB_HEIGHT
-    };
+    var THUMB_HEIGHT = 150,
+        RESCALED_WIDTH = 360;
 
-    if (dst.thumbWidth / dst.thumbHeight < 4 / 6) {
-        dst.thumbWidth = parseInt(4 / 6 * dst.thumbHeight, 10);
-    } else if (dst.thumbWidth / dst.thumbHeight > 6 / 4) {
-        dst.thumbWidth = parseInt(6 / 4 * dst.thumbHeight, 10);
-    }
+    gmInst.size(function(err, source) {
 
-    dst.thumbRatio = dst.thumbWidth / dst.thumbHeight;
+        var rescaled, cropped, thumb;
 
-    var crp = {};
+        source.ratio = source.width / source.height;
 
-    if (dst.thumbRatio < src.ratio) {
-        console.log('case 1');
-        crp.height = src.height;
-        crp.width = parseInt(crp.height * dst.thumbRatio, 10);
-        crp.x = parseInt((src.width - crp.width) / 2, 10);
-        crp.y = 0;
-    } else {
-        crp.width = src.width;
-        crp.height = parseInt(crp.width / dst.thumbRatio, 10);
-        crp.x = 0;
-        crp.y = parseInt((src.height - crp.height) / 2, 10);
-    }
+        rescaled = {
+            width: RESCALED_WIDTH,
+            height: parseInt(RESCALED_WIDTH / source.ratio, 10)
+        };
 
+        cropped = {
+            width: source.width,
+            height: source.height,
+            x: 0,
+            y: 0
+        };
 
+        thumb = {
+            ratio: null,
+            width: parseInt(THUMB_HEIGHT * source.ratio, 10),
+            height: THUMB_HEIGHT
+        };
 
+        if (thumb.width / thumb.height < 4 / 6) {
+            thumb.width = parseInt(4 / 6 * thumb.height, 10);
+        } else if (thumb.width / thumb.height > 6 / 4) {
+            thumb.width = parseInt(6 / 4 * thumb.height, 10);
+        }
+        thumb.ratio = thumb.width / thumb.height;
 
+        if (thumb.ratio < source.ratio) {
+            cropped.width = parseInt(cropped.height * thumb.ratio, 10);
+            cropped.x = parseInt((source.width - cropped.width) / 2, 10);
+        } else {
+            cropped.height = parseInt(cropped.width / thumb.ratio, 10);
+            cropped.y = parseInt((source.height - cropped.height) / 2, 10);
+        }
 
-    gmImg.autoOrient()
-        .crop(crp.width, crp.height, crp.x, crp.y)
-        .thumb(dst.thumbWidth, dst.thumbHeight, './test/sample_test.jpg', 70, function() {});
-
-});
-/*.resize(20, 20)
-.write('./test/sample_test.jpg', function(err) {
-	if (!err) {
-	console.log('done');
+        gm(imgPath).autoOrient().thumb(rescaled.width, rescaled.height, './test/sample_test1.jpg', 70, function() {});
+        gmInst.crop(cropped.width, cropped.height, cropped.x, cropped.y)
+            .thumb(thumb.width, thumb.height, './test/sample_test.jpg', 70, function() {});
+    });
 }
-});*/
